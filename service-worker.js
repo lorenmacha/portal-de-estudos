fbc516eb0d7c4ec225b4a545b0afc3cf6ec5e436
Lorenzo Félix Machado <lorenzo.felix@ufrgs.br>
2026-09-25 11:17:55 -0300

    Atualiza cache para destaque dos Nobel de Economia

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='portal-estudos-web-v15';
+const CACHE='portal-estudos-web-v16';
 const FILES=[
   './','./index.html','./atlas-das-ideias.html','./atlas-economia-politica.html','./site.webmanifest','./site-manifest.json','./MANUAL-DE-USO.html',
   './assets/search-index.js','./assets/jszip.min.js','./assets/web-backend.js','./assets/guide-enhancements.js','./assets/guide-enhancements.css',
