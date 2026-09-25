f1620cbd3085ca262573196ff9f6bcbcca7c8140
Lorenzo Félix Machado <lorenzo.felix@ufrgs.br>
2026-09-25 18:17:24 -0300

    Atualiza cache após expansão do Atlas de PTGE

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='portal-estudos-web-v20';
+const CACHE='portal-estudos-web-v21';
 const FILES=[
   './','./index.html','./atlas-das-ideias.html','./atlas-economia-politica.html','./atlas-ptge.html','./site.webmanifest','./site-manifest.json','./MANUAL-DE-USO.html',
   './assets/search-index.js','./assets/jszip.min.js','./assets/web-backend.js','./assets/guide-enhancements.js','./assets/guide-enhancements.css',
